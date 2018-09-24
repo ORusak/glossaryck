87fea63f85db73c982c03ef53354eed8a16a94cc
Oleg Rusak <oleg.rusak@nordigy.ru>
2018-09-24 14:35:36 +0300

    add test custom timeout

--- src/sandbox/lib/subscribe-promisify/test/subscribe-promisify.js
+++ src/sandbox/lib/subscribe-promisify/test/subscribe-promisify.js
@@ -3,7 +3,7 @@
 
 const test = require('ava');
 
-const SubcribePromisify = require('../subscribe-promisify');
+const SubscribePromisify = require('../subscribe-promisify');
 
 test.before(() => {
 
@@ -13,45 +13,48 @@ test.after(() => {
 })
 
 test('SubscribePromisify.mainHandler',  async (t) => {
-    const supromise = new SubcribePromisify();
+    const supromise = new SubscribePromisify();
     
     const promise = supromise.register((yeah, nah, ...args) => {
-        console.log(...args);
         const [ message ] = args;
 
-        console.log(message);
-        
         return yeah(1);
     });
 
     const intervalId = setTimeout(supromise.handler, 1000, { type: 'UPDATED' });
-    // const intervalId = setInterval((...args) => {
-    //     console.log(args);
-
-    //     clearInterval(intervalId);
-    //     t.end();
-    // }, 1000, { type: 'UPDATED' });
     
     const result = await promise;
         
     t.true(result === 1);
 });
 
-test('SubscribePromisify.mainHandler timeout',  async (t) => {
-    const supromise = new SubcribePromisify();
+test('SubscribePromisify.mainHandler default timeout',  async (t) => {
+    const supromise = new SubscribePromisify();
     
     const promise = supromise.register((yeah, nah, ...args) => {
+        //  never
         if (false) return yeah()
     });
 
     const intervalId = setTimeout(supromise.handler, 0, { type: 'UPDATED' });
-    // const intervalId = setInterval((...args) => {
-    //     console.log(args);
+        
+    try {
+        const result = await promise;
+    } catch (error) {
+        t.is(error.message, 'timeout error')
+    }
+});
 
-    //     clearInterval(intervalId);
-    //     t.end();
-    // }, 1000, { type: 'UPDATED' });
+test('SubscribePromisify.mainHandler custom timeout',  async (t) => {
+    const supromise = new SubscribePromisify();
     
+    const promise = supromise.register((yeah, nah, ...args) => {
+        //  never
+        if (false) return yeah()
+    }, 100);
+
+    const intervalId = setTimeout(supromise.handler, 0, { type: 'UPDATED' });
+        
     try {
         const result = await promise;
     } catch (error) {
